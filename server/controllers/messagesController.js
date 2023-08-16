b500264c5d00f8005446f2a667110811d9cf615d
Sahil Panchbhaiya <onlinecodingsahil@gmail.com>
2023-08-16 18:48:29 +0530

    Pushed server

---
+++ server/controllers/messagesController.js
@@ -0,0 +1,44 @@
+const messageModel = require("../models/messageModel");
+
+module.exports.addMessage = async(req,res,next) => {
+    try{
+
+        const {from ,to , message} = req.body;
+        const data = await messageModel.create({
+            message:{
+                text:message,
+            },
+            users:[from , to],
+            sender:from,
+        })
+        if(data){
+            return res.json({msg :"Message Added Successfully"});
+        }
+        else{
+            return res.json({msg :"Failed To Add Message To Database"});
+        }
+    }catch(err){
+        next(err);
+    }
+    
+}
+module.exports.getAllMessage = async(req,res,next) => {
+    try{
+        const {from , to } = req.body;
+        const messages = await messageModel.find({
+            users:{
+                $all:[from, to ],
+            }
+        }).sort({updatedAt:1});
+
+        const projectedMessages = messages.map((msg) => {
+                return {
+                    fromSelf:msg.sender.toString()===from,
+                    message:msg.message.text,
+                }
+        })
+            return res.json(projectedMessages);
+    }catch(er){
+        next(er);
+    }
+}
